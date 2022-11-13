fc54636d8397f828f28f57ccd9ddb1d5e0fa92cc
Maor Levy <maor@firstdag.com>
2022-11-13 19:55:58 +0200

    Removed old unused files

--- web/src/components/main/Modal.js
+++ web/src/components/main/Modal.js
@@ -1,55 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ModalContext } from '../../contexts/ModalContext';
 import { clearModalAction, goBackAction } from '../../actions/ModalActions';
-import ShowMovieDetails from '../theaters/ShowMovieDetails';
-import Seats from '../theaters/Seats';
-import Reservation from '../theaters/Reservation';
 import Default from './Default';
-import AddComment from '../movies/AddComment';
-import Movie from '../movies/Movie';
 import LoginPage from '../login/LoginPage';
 import Menu from '../account/Menu';
-import UpdateMovie from '../account/admin/movies/UpdateMovie';
-import UpdateTheater from '../account/admin/theaters/UpdateTheater';
 import UpdateItem from '../account/admin/UpdateItem';
-import UpdateTheaterStats from '../account/admin/theaters/UpdateTheaterStats';
-import UpdateAvailableTimeSlots from '../account/admin/theaters/UpdateAvailableTimeSlots';
-import AddLoction from '../account/admin/theaters/AddLoction';
-import AddTheater from '../account/admin/theaters/AddTheater';
-import AddNewTheaterStats from '../account/admin/theaters/AddNewTheaterStats';
-import DeleteLocation from '../account/admin/theaters/DeleteLocation';
 import ConfirmDelete from '../account/admin/ConfirmDelete';
-import DeleteTheater from '../account/admin/theaters/DeleteTheater';
-import DeleteTheaters from '../account/admin/theaters/DeleteTheaters';
-import AddArticle from '../account/admin/news/AddArticle';
-import UpdateArticle from '../account/admin/news/UpdateArticle';
-import UpdateArticleStats from '../account/admin/news/UpdateArticleStats';
-import DeleteArticle from '../account/admin/news/DeleteArticle';
-import AddMovie from '../account/admin/movies/AddMovie';
-import DeleteMovies from '../account/admin/movies/DeleteMovies';
-import AddMovieTimeSlots from '../account/admin/movies/AddMovieTimeSlots';
 import ChangePassword from '../account/admin/account/ChangePassword';
 import AddAccount from '../account/admin/account/AddAccount';
 import DeleteAccount from '../account/admin/account/DeleteAccount';
 import AdjustItems from '../account/admin/AdjustItems';
-import AddTimeSlots from '../account/admin/movies/AddTimeSlots';
-import UpdateMovieStats from '../account/admin/movies/UpdateMovieStats';
 import ApprovalMessage from './ApprovalMessage';
 export default function Modal() {
     const { modalData, modalDataDispatch } = useContext(ModalContext);
     const components = {
         Default, LoginPage, ApprovalMessage,
-        ShowMovieDetails, Seats, Reservation,
-        AddComment, Movie,
-        Menu, UpdateMovie, AddMovie, DeleteMovies, AddMovieTimeSlots, AddTimeSlots,
-        UpdateMovieStats,
+        Menu,
         ConfirmDelete, UpdateItem, AdjustItems,
-        UpdateTheater, UpdateTheaterStats, UpdateAvailableTimeSlots, AddLoction,
-        AddTheater, AddNewTheaterStats, DeleteLocation, DeleteTheater, DeleteTheaters,
-
-
-        AddArticle, UpdateArticle, UpdateArticleStats, DeleteArticle,
-
         ChangePassword, AddAccount, DeleteAccount
     };
     const [children, setChildren] = useState({ elementName: 'Default', props: {} });
--- web/src/components/movies/Trailer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Trailer({ trailer }) {
-    return (
-        <iframe className="trailer" src={trailer} title="movie trailer from youtube"
-            frameborder="0" allowfullscreen>
-        </iframe>
-    )
-}
