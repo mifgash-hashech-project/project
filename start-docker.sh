4973026709aa1091b6afd7c54ac4d7c706389c6e
Maor Levy <maor@firstdag.com>
2022-12-28 01:08:14 +0200

    Added postgresSQL

--- start-docker.sh
+++ start-docker.sh
@@ -9,6 +9,11 @@ export SCHEDULE_HOST=schedule
 export AUTH_PORT=3003
 export SHIFTS_PORT=3006
 export MONGODB_PORT=27017
+export POSTGRES_HOST=postgres
+export POSTGRES_USER=master
+export POSTGRES_PASSWORD=password
+export POSTGRES_DB=sheikh
+export POSTGRES_PORT=5432
 
 
 docker-compose -f docker/docker-compose.yml up $2
