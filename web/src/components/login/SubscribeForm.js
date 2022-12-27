e7837c02df44098d4f5a2228d7adea040efa1136
Maor Levy <maor@firstdag.com>
2022-12-27 23:09:36 +0200

    Modified subscribe form (without server-side)

--- web/src/components/login/SubscribeForm.js
+++ web/src/components/login/SubscribeForm.js
@@ -1,24 +1,27 @@
-import React, { useContext, useState } from 'react';
+import React, {useContext, useState} from 'react';
 import validator from 'validator';
-import { setDataAction } from '../../actions/DataActions';
-import { clearModalAction, goForwardAction } from '../../actions/ModalActions';
-import { loginAction } from '../../actions/UserActions';
-import { DataContext } from '../../contexts/DataContext';
-import { ModalContext } from '../../contexts/ModalContext';
-import { UserContext } from '../../contexts/UserContext';
-import { subscribe } from '../../server/login';
-import { getAdminsData } from '../../server/utils';
+import {setDataAction} from '../../actions/DataActions';
+import {clearModalAction, goForwardAction} from '../../actions/ModalActions';
+import {loginAction} from '../../actions/UserActions';
+import {DataContext} from '../../contexts/DataContext';
+import {ModalContext} from '../../contexts/ModalContext';
+import {UserContext} from '../../contexts/UserContext';
+import {subscribe} from '../../server/login';
+import {getAdminsData} from '../../server/utils';
 
 const SubscribeForm = (props) => {
-    const { userData, userDataDispatch } = useContext(UserContext);
-    const { modalDataDispatch } = useContext(ModalContext);
-    const { contentDataDispatch } = useContext(DataContext);
-    const [inputClasses, setInputClasses] = useState(["", "", "", ""]);
-    const [invalidMessages, setInvalidMessages] = useState(["", "", "", ""]);
-    const [validInputs, setValidInputs] = useState([false, false, false, false]);
+    const {userData, userDataDispatch} = useContext(UserContext);
+    const {modalDataDispatch} = useContext(ModalContext);
+    const {contentDataDispatch} = useContext(DataContext);
+    const [inputClasses, setInputClasses] = useState(["", "", "", "", "", ""]);
+    const [invalidMessages, setInvalidMessages] = useState(["", "", "", "", "", ""]);
+    const [validInputs, setValidInputs] = useState([false, false, false, false, false, false]);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
+    const rolePlaceHolderValue = "Pick a Role:"
+    const [role, setRole] = useState(rolePlaceHolderValue);
+    const [phone, setPhone] = useState("");
 
 
     const isFormInvalid = () => {
@@ -57,6 +60,37 @@ const SubscribeForm = (props) => {
         } else {
             setStateOfInputs(missingValueMessage, "input-invalid", false, inputindex);
         }
+    };
+
+    const onBlurRole = (event) => {
+        const newRole = event.target.value;
+        const isRoleValid = (value) => {
+            return value !== rolePlaceHolderValue;
+        };
+        validateInput(
+            newRole,
+            4,
+            isRoleValid,
+            setRole,
+            "You must pick a role",
+            "You must pick a role"
+        );
+    };
+
+    const onBlurPhone = (event) => {
+        const newPhoneNumber = event.target.value.trim();
+        function isValidPhoneNumber(phoneNumber) {
+            const phoneNumberPattern = /^05\d{8}$/;
+            return phoneNumberPattern.test(phoneNumber);
+        }
+        validateInput(
+            newPhoneNumber,
+            5,
+            isValidPhoneNumber,
+            setPhone,
+            "You must enter a phone number",
+            'Phone number must start with 05x and have 10 digits, no hyphens ("-").'
+        );
     };
 
     const onBlurUsername = (event) => {
@@ -112,7 +146,8 @@ const SubscribeForm = (props) => {
             passwordRepeated,
             3,
             isPasswordRepeatedValid,
-            () => { },
+            () => {
+            },
             "You must enter again your password",
             "The two passwords not identical"
         );
@@ -121,7 +156,7 @@ const SubscribeForm = (props) => {
     const onSubmitform = async (event) => {
         event.preventDefault();
         try {
-            const request = { email, password, name: username, isAdmin: !props.partOfLogin };
+            const request = {email, password, name: username, isAdmin: !props.partOfLogin};
             const subscribeData = await subscribe(request);
             if (props.partOfLogin) userDataDispatch(loginAction(subscribeData, false));
             else {
@@ -131,7 +166,7 @@ const SubscribeForm = (props) => {
             modalDataDispatch(clearModalAction());
             modalDataDispatch(goForwardAction({
                 elementName: "ApprovalMessage",
-                props: { message: "Account successfully added!" }
+                props: {message: "Account successfully added!"}
             }));
 
         } catch (err) {
@@ -150,14 +185,25 @@ const SubscribeForm = (props) => {
         <div className="login-form">
             <h3>Subscribe</h3>
             <form onSubmit={onSubmitform}>
-                <input placeholder="Username" className={inputClasses[0]} onBlur={onBlurUsername} />
+                <input placeholder="Username" className={inputClasses[0]} onBlur={onBlurUsername}/>
                 {invalidMessages[0] !== "" && <div className="invalid-message">{invalidMessages[0]}</div>}
-                <input placeholder="Email" className={inputClasses[1]} onBlur={onBlurEmail} />
+                <input placeholder="Email" className={inputClasses[1]} onBlur={onBlurEmail}/>
                 {invalidMessages[1] !== "" && <div className="invalid-message">{invalidMessages[1]}</div>}
-                <input type="password" placeholder="Password" className={inputClasses[2]} onBlur={onBlurPassword} />
+                <input type="password" placeholder="Password" className={inputClasses[2]} onBlur={onBlurPassword}/>
                 {invalidMessages[2] !== "" && <div className="invalid-message">{invalidMessages[2]}</div>}
-                <input type="password" placeholder="Repeat on password" className={inputClasses[3]} onBlur={onBlurPasswordRepeated} />
+                <input type="password" placeholder="Repeat on password" className={inputClasses[3]}
+                       onBlur={onBlurPasswordRepeated}/>
                 {invalidMessages[3] !== "" && <div className="invalid-message">{invalidMessages[3]}</div>}
+                <select value={role} className={inputClasses[4]} onChange={onBlurRole}>
+                    <option value={rolePlaceHolderValue}/>
+                    <option value="Seller"/>
+                    <option value="Cook"/>
+                    <option value="Manager"/>
+                </select>
+                {invalidMessages[4] !== "" && <div className="invalid-message">{invalidMessages[4]}</div>}
+                <input placeholder="Enter a phone number" className={inputClasses[5]}
+                       onBlur={onBlurPhone}/>
+                {invalidMessages[5] !== "" && <div className="invalid-message">{invalidMessages[5]}</div>}
                 <div className="login-form__nav">
                     <button type="submit" disabled={isFormInvalid()}>Submit</button>
                     {props.partOfLogin && <div className="login-or-subscribe" onClick={onClickLogin}>Login</div>}
