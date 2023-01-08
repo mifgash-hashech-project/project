30869a31ad4b34ff7555c23fcb31a999e6e0711d
Maor Levy <maor@firstdag.com>
2023-01-08 12:01:40 +0200

    Added default values for auth service

--- auth/src/keys/keys.js
+++ auth/src/keys/keys.js
@@ -1,12 +1,12 @@
 const keys = {
-    port: process.env.PORT,
-    mongodbHost: process.env.MONGODB_HOST,
-    mongodbPort: process.env.MONGODB_PORT,
-    mongodbUser: process.env.MONGO_INITDB_ROOT_USERNAME,
-    mongodbPassword: process.env.MONGO_INITDB_ROOT_PASSWORD,
-    mongoCollection: process.env.MONGODB_COLLECTION,
-    employeesHost: process.env.EMPLOYEES_HOST,
-    employeesPort: process.env.EMPLOYEES_PORT,
+    port: process.env.PORT || 3003,
+    mongodbHost: process.env.MONGODB_HOST || "localhost",
+    mongodbPort: process.env.MONGODB_PORT || 27017,
+    mongodbUser: process.env.MONGO_INITDB_ROOT_USERNAME || "master",
+    mongodbPassword: process.env.MONGO_INITDB_ROOT_PASSWORD || "password",
+    mongoCollection: process.env.MONGODB_COLLECTION || "users",
+    employeesHost: process.env.EMPLOYEES_HOST || "localhost",
+    employeesPort: process.env.EMPLOYEES_PORT || 3001,
     jwtSecret: process.env.JWT_SECRET || "ThisIsMySecret123!",
 }
 
